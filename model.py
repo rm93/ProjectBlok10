0408af975de0f505ce8d307965c8cb4a6e30f30d
william <willysieswilly@gmail.com>
2018-03-23 23:33:17 +0100

    model fix, testing on the train needs to be performed and the test for the model needs to be writen

--- model.py
+++ model.py
@@ -63,32 +63,37 @@ def convolutional_neural_network(x):
 #train sequence with input x = data
 def train_neural_network(x):
     print(" test ")
+    numOfImages = tf.size(x)
     #prediction with model
     prediction = convolutional_neural_network(x)
-    print(prediction)
-    print(tf.nn.softmax_cross_entropy_with_logits(prediction, y))
 
     #calculate cost
-    # cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(prediction, y))
-    # optimizer = tf.train.AdamOptimizer().minimize(cost)
-    #
-    # hm_epochs = 10
-    # with tf.Session() as sess:
-    #     sess.run(tf.initialize_all_variables())
-    #
-    #     for epoch in range(hm_epochs):
-    #         epoch_loss = 0
-    #         for _ in range(int(mnist.train.num_examples / batch_size)):
-    #             epoch_x, epoch_y = mnist.train.next_batch(batch_size)
-    #             _, c = sess.run([optimizer, cost], feed_dict={x: epoch_x, y: epoch_y})
-    #             epoch_loss += c
-    #
-    #         print('Epoch', epoch, 'completed out of', hm_epochs, 'loss:', epoch_loss)
-    #
-    #     correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
-    #
-    #     accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
-    #     print('Accuracy:', accuracy.eval({x: mnist.test.images, y: mnist.test.labels}))
+    new_y = tf.cast(y, tf.int32)
+    entropy = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels=new_y, logits=prediction))
+    optimizer = tf.train.AdamOptimizer().minimize(entropy)
 
+    hm_epochs = 10
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+
+        for epoch in range(hm_epochs):
+            epoch_loss = 0
+            i = 0
+            while i < len(x):
+                start = i
+                end = i + batch_size
+                batch_x = np.array(x[start:end])
+                batch_y = np.array(y[start:end])
+
+                _, c = sess.run([optimizer, entropy], feed_dict={x: batch_x,
+                                                              y: batch_y})
+                epoch_loss += c
+                i += batch_size
+
+            print('Epoch', epoch + 1, 'completed out of', hm_epochs, 'loss:', epoch_loss)
+            correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
+            accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
+
+            print('Accuracy:', accuracy.eval({x: x, y: y}))
 def test_neural_network(x):
     return
