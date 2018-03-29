77347cda00972e86582339e8d2f8b06c0df71922
rm93 <rmedemblik93@gmail.com>
2018-03-29 19:44:58 +0200

    Complete script

--- cnn.py
+++ cnn.py
@@ -1,30 +1,66 @@
-#Opencv-contrib-python, tqdm, pandas, sklearn, scipy, tensorflow, tflearn
-import os
+#Opencv-contrib-python, tqdm, pandas, sklearn, scipy, tensorflow, tflearn, keras, numpy, h5py
 
 import cv2
-import numpy as np
+import os
 import pandas as pd
+import numpy as np
+from tqdm import tqdm
+from keras.callbacks import EarlyStopping
+from keras.callbacks import TensorBoard
+from keras.layers import Dense, Activation, Flatten, Dropout
+from keras.layers import MaxPooling2D
+from keras.layers.convolutional import Conv2D
+from keras.models import Sequential
+from keras.utils import np_utils
+from keras.models import load_model
+from sklearn.metrics import recall_score
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
-from tqdm import tqdm
-
 
 def main():
-    ImgSize = 512
+    global path
+    global nameInputCsv
+    global namecsv
+    global iArray
 
-    ResizeImages(path='../ProjectBlok10/data/images/', new_path='../ProjectBlok10/data/resized-'+str(ImgSize)+'/', img_size=ImgSize)
+    ImgSize = 128
+    batch_size = 100
+    nb_classes = 8
+    nb_epoch = 20
+    channels = 1
+    nb_filters = 32
+    kernel_size = (2, 2)
+
+    #Path data directory
+    path = "../test_projectblok10/data/"
+    #Name resized folder
+    resizedName = 'resized-'+str(ImgSize)+'/'
+    #Name input csv
+    nameInputCsv = "reduced_sample_labels"
+    #Name output csv
+    namecsv = "new_sample_labels"
+    #Name x image array
+    iArray = "X_imageArray-"+str(ImgSize)
+    #Name results csv
+    nameResCsv = "testResults"
+    #Name Model
+    #MODEL_NAME = 'Model-{}-{}-{}.h5'.format(ImgSize, nb_epoch, batch_size)
+    MODEL_NAME = 'Projectblok10-{}-{}.h5'.format(ImgSize, nb_epoch)
+
+    ResizeImages(path=path+'/reduced_images/', new_path=path+resizedName, img_size=ImgSize)
     CreateNewCsv(ImgSize)
     CreateImageArray(ImgSize)
-    X_train, y_train, X_test, y_test = SplitData()
-    cnn_model()
-    Predict()
+    X_train, y_train, X_test, y_test = SplitData(ImgSize, channels, nb_classes)
+    model = cnn_model(X_train, y_train, kernel_size, nb_filters, channels, nb_epoch, batch_size, nb_classes, ImgSize, MODEL_NAME, path)
+    y_test, y_pred = Predict(model, X_test, y_test)
+    MakeCsv(y_test, y_pred, nameResCsv, path)
 
 
 def ResizeImages(path, new_path, img_size):
     #Crops, resizes, and stores all images from a directory in a new directory.
 
     if not os.path.exists(new_path):
-        print("Resize the images to: {}px by {}px for further use.\n".format(img_size, img_size))
+        print("Resize the images to: {}px by {}px for further use.".format(img_size, img_size))
         os.makedirs(new_path)
         dirs = [l for l in os.listdir(path) if l != '.DS_Store']
 
@@ -41,9 +77,9 @@ def ResizeImages(path, new_path, img_size):
 
 def CreateNewCsv(ImgSize):
     #Reads in the csv with al the info and edit the file to save only the needed info.
-    if not os.path.exists('../ProjectBlok10/data/new_sample_labels.csv'):
-        data = pd.read_csv("../ProjectBlok10/data/sample_labels.csv")
-        sample = os.listdir('../ProjectBlok10/data/resized-'+str(ImgSize)+'/')
+    if not os.path.exists(path+namecsv+'.csv'):
+        data = pd.read_csv(path+nameInputCsv+".csv")
+        sample = os.listdir(path+'resized-'+str(ImgSize)+'/')
 
         sample = pd.DataFrame({'Image_Index': sample})
 
@@ -62,26 +98,26 @@ def CreateNewCsv(ImgSize):
 
         print("Saving CSV")
 
-        sample.to_csv('../ProjectBlok10/data/new_sample_labels.csv', index=False, header=True)
+        sample.to_csv(path+namecsv+'.csv', index=False, header=True)
     else:
         print("The labels are already processed")
 
 def CreateImageArray(ImgSize):
     #Converts each image to an array, and appends each array to a new NumPy array, based on the image column equaling the image file name.
-    if not os.path.exists('../ProjectBlok10/data/X_sample.npy'):
+    if not os.path.exists(path+iArray+'.npy'):
         print("Processing image array")
-        labels = pd.read_csv("../ProjectBlok10/data/new_sample_labels.csv")
+        labels = pd.read_csv(path+namecsv+".csv")
         lst_imgs = [l for l in labels['Image_Index']]
-        X_train = np.array([np.array(cv2.imread('../ProjectBlok10/data/resized-'+str(ImgSize)+'/' + img, cv2.IMREAD_GRAYSCALE)) for img in tqdm(lst_imgs)])
+        X_train = np.array([np.array(cv2.imread(path+'resized-'+str(ImgSize)+'/' + img, cv2.IMREAD_GRAYSCALE)) for img in tqdm(lst_imgs)])
         print("Saving image array")
-        np.save('../ProjectBlok10/data/X_sample.npy', X_train)
+        np.save(path+iArray+'.npy', X_train)
     else:
         print("The image array is already processed")
 
-def SplitData():
+def SplitData(ImgSize, channels, nb_classes):
     # Import data
-    labels = pd.read_csv("../ProjectBlok10/data/sample_labels.csv")
-    X = np.load("../ProjectBlok10/data/X_sample.npy")
+    labels = pd.read_csv(path+namecsv+".csv")
+    X = np.load(path+iArray+".npy")
 
     y = labels.Finding_Labels
     label_encoder = LabelEncoder()
@@ -91,12 +127,151 @@ def SplitData():
     print("The data is split into train and test data sets")
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
+    print("Reshaping Data")
+    X_train = X_train.reshape(X_train.shape[0], ImgSize, ImgSize, channels)
+    X_test = X_test.reshape(X_test.shape[0], ImgSize, ImgSize, channels)
+
+    print("X_train Shape: ", X_train.shape)
+    print("X_test Shape: ", X_test.shape)
+
+    print("Normalizing Data")
+    X_train = X_train.astype('float32')
+    X_test = X_test.astype('float32')
+
+    X_train /= 255
+    X_test /= 255
+
+    y_train = np_utils.to_categorical(y_train, nb_classes)
+    y_test = np_utils.to_categorical(y_test, nb_classes)
+    print("y_train Shape: ", y_train.shape)
+    print("y_test Shape: ", y_test.shape)
+
     return X_train, y_train, X_test, y_test
 
-def cnn_model():
-    None
+def cnn_model(X_train, y_train, kernel_size, nb_filters, channels, nb_epoch, batch_size, nb_classes, ImgSize, MODEL_NAME, path):
 
-def Predict():
-    None
+    if not os.path.exists(path+MODEL_NAME):
+        model = Sequential()
+
+        '''
+        First set of three layers
+        Image size: 256 x 256
+        nb_filters = 32
+        kernel_size = (2,2)
+        '''
+
+        model.add(Conv2D(nb_filters, (kernel_size[0], kernel_size[1]),
+                         padding='valid',
+                         strides=1,
+                         input_shape=(ImgSize, ImgSize, channels)))
+        model.add(Activation('relu'))
+
+        model.add(Conv2D(nb_filters, (kernel_size[0], kernel_size[1])))
+        model.add(Activation('relu'))
+
+        model.add(Conv2D(nb_filters, (kernel_size[0], kernel_size[1])))
+        model.add(Activation('relu'))
+
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+
+        '''
+        Second set of three layers
+        Image Size: 128 x 128
+        nb_filters = 64
+        kernel_size = 4,4
+        '''
+
+        nb_filters = 64
+        kernel_size = (4, 4)
+
+        model.add(Conv2D(nb_filters, (kernel_size[0], kernel_size[1])))
+        model.add(Activation('relu'))
+
+        model.add(Conv2D(nb_filters, (kernel_size[0], kernel_size[1])))
+        model.add(Activation('relu'))
+
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+
+        '''
+        Third set of three layers
+        Image Size: 64 x 64
+        nb_filters = 128
+        kernel_size = 8,8
+        '''
+
+        nb_filters = 128
+        kernel_size = (8, 8)
+
+        model.add(Conv2D(nb_filters, (kernel_size[0], kernel_size[1])))
+        model.add(Activation('relu'))
+
+        model.add(Conv2D(nb_filters, (kernel_size[0], kernel_size[1])))
+        model.add(Activation('relu'))
+
+        model.add(MaxPooling2D(pool_size=(12, 12)))
+
+        model.add(Flatten())
+        print("Model flattened out to: ", model.output_shape)
+
+        model.add(Dense(4096))
+        model.add(Activation('relu'))
+        model.add(Dropout(0.2))
+
+        model.add(Dense(4096))
+        model.add(Activation("relu"))
+        model.add(Dropout(0.2))
+
+        model.add(Dense(nb_classes))
+        model.add(Activation('softmax'))
+
+        model.compile(loss='categorical_crossentropy',
+                      optimizer='adam',
+                      metrics=['accuracy'])
+
+        print(model.summary())
+
+        stop = EarlyStopping(monitor='acc',
+                             min_delta=0.001,
+                             patience=2,
+                             verbose=0,
+                             mode='auto')
+
+        tensor_board = TensorBoard(log_dir='./Log', histogram_freq=0, write_graph=True, write_images=True)
+
+        model.fit(X_train, y_train, batch_size=batch_size, epochs=nb_epoch,
+                  verbose=1,
+                  validation_split=0.2,
+                  class_weight='auto',
+                  callbacks=[stop, tensor_board]
+                  )
+
+        model.save(path+MODEL_NAME)
+        return model
+    else:
+        model = load_model(path+MODEL_NAME)
+        print('Model loaded!')
+        return model
+
+def Predict(model, X_test, y_test):
+    print("Predicting")
+
+    y_pred = model.predict(X_test)
+
+    y_test = np.argmax(y_test, axis=1)
+    y_pred = np.argmax(y_pred, axis=1)
+
+    recall = recall_score(y_test, y_pred, average='weighted')
+    print("Accuracy: ", round(recall*100, 2), "%")
+
+    return y_test, y_pred
+
+def MakeCsv(y_test, y_pred, nameResCsv, path):
+    with open(path+nameResCsv+".csv", "w") as f:
+        f.write("Label\tPredicted label\n")
+
+    print("Making results csv")
+    with open(path+nameResCsv+".csv", "a") as f:
+        for i in tqdm(range(len(y_test))):
+            f.write("{}\t{}\n".format(y_test[i], y_pred[i]))
 
 main()
